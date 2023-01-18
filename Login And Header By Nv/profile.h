aa9d7510fa1e6db2ff73c32485ad1fcb51879d81
Novvx <122433421+Novv111@users.noreply.github.com>
2023-01-18 12:55:51 +0700

    Add files via upload

---
+++ Login And Header By Nv/profile.h
@@ -0,0 +1,16 @@
+pROFILE (char jud[50]="")
+{
+if (strcmp(jud,"PROFILE")==0)
+ {
+cout<<"---------------+ PROFILE +---------------"<<endl;
+cout<<"Nama       = Ade Nova Wiguna   "<<endl;
+cout<<"Kelas      = X PPLG    "<<endl;
+cout<<"Alamat     = Mars      "<<endl;
+cout<<"No Ponsel  = 0858696969 "<<endl;
+cout<<"Username   = Claire     "<<endl;
+cout<<"Password   = Kafucin   "<<endl;
+cout<<"---------------+ PROFILE +---------------"<<endl;
+cout<<""<<endl;
+}
+}
+
